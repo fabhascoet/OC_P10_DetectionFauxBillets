ca6dc8f99c6ab70b878bf778b435bc5c1773c978
fabhascoet <96290814+fabhascoet@users.noreply.github.com>
2023-02-10 10:37:48 +0100

    Update App_P10.py

--- App_P10.py
+++ App_P10.py
@@ -2,5 +2,20 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.subheader("Projet 10 : Détection de faux billets")
+# Eléments de titre
+st.subheader("Projet 10 : Détection de faux billets 💵")
+st.image(https://1drv.ms/u/s!AorQVY2bdCT_gtUuO8iKy0g5l_eIQA?e=eaQvsX)
 st.write("Application finale")
+
+# Sidebar pour import des données
+st.sidebar.write("Importer les données à analyser")
+st.sidebar.button("Click me!")
+
+
+
+#st.write("Download button - Display a download button widget")
+#st.download_button("Download file", file)
+
+#st.write("File Uploader - Display a file uploader widget")
+#data = st.file_uploader("Upload a CSV")
+
