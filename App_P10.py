8caaf1493124b26ae4c0a466cb77f33039f0a213
fabhascoet <96290814+fabhascoet@users.noreply.github.com>
2023-02-10 11:25:47 +0100

    Update App_P10.py

--- App_P10.py
+++ App_P10.py
@@ -29,8 +29,14 @@ st.write("Application finale")
 #display(data)
 #data.info()
 
-uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
-for uploaded_file in uploaded_files:
-    bytes_data = uploaded_file.read()
-    st.write("filename:", uploaded_file.name)
-    st.write(bytes_data)
+#uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True)
+#for uploaded_file in uploaded_files:
+    #bytes_data = uploaded_file.read()
+    #st.write("filename:", uploaded_file.name)
+    #st.write(bytes_data)
+
+    
+uploaded_file = st.file_uploader("Choose a file")
+# Can be used wherever a "file-like" object is accepted:
+dataframe = pd.read_csv(uploaded_file)
+st.write(dataframe)
