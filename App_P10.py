9c1a30b8e71a633b331c64c8aae2adfcb6cfec9b
fabhascoet <96290814+fabhascoet@users.noreply.github.com>
2023-02-14 16:23:15 +0100

    Update App_P10.py

--- App_P10.py
+++ App_P10.py
@@ -65,7 +65,8 @@ ID = dataframe["id"]
 
 ## Section 4 : Entraînement du modèle
 #from sklearn.linear_model import LogisticRegression
-from sklearn import LogisticRegression
+#from sklearn import LogisticRegression
+import sklearn as sklearn
 # On choisit "LogisticRegression"
 model_lrg = LogisticRegression()
 # Entraînement du modèle sur les données
